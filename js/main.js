26c055563b1d2c3de1f2d81f689ac8f2515683cd
breno fernandes <fernandesbreno.project@gmail.com>
2023-08-27 14:50:03 -0300

    commit corrigindo ident

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
 const itens = JSON.parse(localStorage.getItem("itens")) || []
 
-itens.forEach( (elemento) => {
+itens.forEach((elemento) => {
     criaElemento(elemento)
-} )
+})
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
@@ -12,7 +12,7 @@ form.addEventListener("submit", (evento) => {
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
-    const existe = itens.find( elemento => elemento.nome === nome.value )
+    const existe = itens.find(elemento => elemento.nome === nome.value)
 
     const itemAtual = {
         "nome": nome.value,
@@ -26,7 +26,7 @@ form.addEventListener("submit", (evento) => {
 
         itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
     } else {
-        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 : 0;
+        itemAtual.id = itens[itens.length - 1] ? (itens[itens.length - 1]).id + 1 : 0;
 
         criaElemento(itemAtual)
 
@@ -40,10 +40,10 @@ form.addEventListener("submit", (evento) => {
 })
 
 function criaElemento(item) {
-    const novoItem = document.createElement("li")
+    const novoItem = document.createElement('li')
     novoItem.classList.add("item")
 
-    const numeroItem = document.createElement("strong")
+    const numeroItem = document.createElement('strong')
     numeroItem.innerHTML = item.quantidade
     numeroItem.dataset.id = item.id
     novoItem.appendChild(numeroItem)
@@ -56,14 +56,14 @@ function criaElemento(item) {
 }
 
 function atualizaElemento(item) {
-    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+    document.querySelector("[data-id='" + item.id + "']").innerHTML = item.quantidade
 }
 
 function botaoDeleta(id) {
     const elementoBotao = document.createElement("button")
     elementoBotao.innerText = "X"
 
-    elementoBotao.addEventListener("click", function() {
+    elementoBotao.addEventListener("click", function () {
         deletaElemento(this.parentNode, id)
     })
 
